7d7cc0d1bce30ccb5b4b9c2a58944a09cc8f55de
MlodyGSportowiec <rockalone233@gmail.com>
2021-10-21 12:29:29 +0200

    stylowanie

--- liga.php
+++ liga.php
@@ -5,23 +5,49 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>piłka nożna</title>
-    <link rel="stylesheet" href="style2.css">
+    <link rel="stylesheet" href="styl2.css">
 </head>
 <body>
     
-    <header id="baner"></header>
+    <header id="baner">
 
         <h3>Reprezentacja polski w piłce nożnej</h3>
         <img src="obraz1.jpg" alt="reprezentacja">
 
-    <section id="lewa"></section>
+    </header>
 
-    
+    <section id="lewy">
 
-    <section id="prawa"></section>
+        <form action="liga.php" method="post"></form>
+        <select>
+            <option>Bramkarze</option>
+            <option>Obrońcy</option>
+            <option>Pomocnicy</option>
+            <option>Napastnicy</option>
+        </select>
+            <input type="submit" value="Zobacz">
+        </form>
+        <br></br>
+        <img src="zad2.png" alt="piłka">
+        <p>Autor: Robert Mrozek 997997997</p>
 
-    <main id="glowny"></main>
+    </section>
 
-    <section id="liga"></section>
+    <section id="prawy">
+
+
+
+    </section>
+
+    <main id="glowny">
+
+        <h3>Liga Mistrzów</p>
+
+    </main>
+    <section id="liga">
+
+
+
+    </section>
 </body>
 </html>
